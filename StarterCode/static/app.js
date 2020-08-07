2a6fd62a91bcba7751b5cd439f8750fc6e1725cf
Taniya Chhabra <chhabra.taniya@gmail.com>
2020-08-06 20:22:36 -0700

    load in data

--- StarterCode/static/app.js
+++ StarterCode/static/app.js
@@ -1,24 +1,3 @@
-d3.json("../samples.json").then(function(data)
+d3.json("../data/samples.json").then(function(data)
 
-{
-    console.log(data); 
-
-    var trace1 = {
-        x: data.sample_values,
-        y: data.otu_ids,
-        type: "bar",
-        orientation: "h"
-    };
-    var data = [trace1];
-
-    var layout = {
-        title: "Belly Button Biodiversity",
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100
-        }
-    };
-    Plotly.newPlot("plot", data, layout);
-});
+{console.log(data)});
